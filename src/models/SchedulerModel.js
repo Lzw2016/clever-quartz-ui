21a7db286cfbb3e849bbacb60e5f31c62c23eb25
李志伟 <lizw@jzteyao.com>
2018-11-29 14:36:55 +0800

    调度器集群

--- src/models/SchedulerModel.js
+++ src/models/SchedulerModel.js
@@ -1,13 +1,24 @@
 // import lodash from 'lodash';
-// import { catIndex } from '../services/index-cat-controller';
+import { allScheduler } from '../services/quartz-scheduler-controller';
 
 export default {
   namespace: 'SchedulerModel',
 
   state: {
+    queryParam: {
+    },
+    data: [],
   },
 
   effects: {
+    *findByPage({ payload }, { select, call, put }) {
+      let queryParam = yield select(state => state.SchedulerModel.queryParam);
+      queryParam = { ...queryParam, ...payload }
+      // 请求数据
+      const data = yield call(allScheduler, queryParam);
+      if (!data) return;
+      yield put({ type: 'save', payload: { data, queryParam } });
+    },
   },
 
   reducers: {
--- src/routes/Quartz/Scheduler.js
+++ src/routes/Quartz/Scheduler.js
@@ -1,24 +1,92 @@
-import React, { PureComponent } from 'react';
-import { Card } from 'antd';
+import React, { PureComponent, Fragment } from 'react';
+import { Card, Form, Row, Button, Table } from 'antd';
 import { connect } from 'dva';
 // import moment from 'moment';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 // import { LocaleLanguage, SystemInfo } from '../../utils/constant';
 // import { changeLocale } from '../../utils/utils';
+import { ModelInitState } from '../../utils/constant';
+// import { SorterOrderMapper } from '../../utils/enum';
 // import classNames from 'classnames';
-// import styles from './Scheduler.less';
+import styles from './Scheduler.less';
 
 @connect(({ SchedulerModel, loading }) => ({
   SchedulerModel,
-  queryLoading: loading.effects['global/queryPage'],
+  queryLoading: loading.effects['SchedulerModel/findByPage'],
 }))
+@Form.create()
 export default class Scheduler extends PureComponent {
+
+  // 数据初始化
+  componentDidMount() {
+    this.findByPage();
+  }
+
+  // 查询数据
+  findByPage = (e) => {
+    if (e) e.preventDefault();
+    const { dispatch, form } = this.props;
+    const queryParam = form.getFieldsValue();
+    dispatch({ type: 'SchedulerModel/findByPage', payload: { ...queryParam, pageNo: 0 } });
+  }
+
+  // 查询表单
+  queryForm() {
+    const {
+      // form: { getFieldDecorator },
+      queryLoading,
+      // SchedulerModel: { queryParam },
+    } = this.props;
+    return (
+      <Form onSubmit={this.findByPage} layout="inline" className={styles.queryForm}>
+        <Row gutter={{ md: 0, lg: 0, xl: 0 }}>
+          <Form.Item className={styles.formItemButton}>
+            <Button type="primary" htmlType="submit" disabled={queryLoading}>查询</Button>
+          </Form.Item>
+        </Row>
+      </Form>
+    );
+  }
+
+  // 数据表格
+  getTable() {
+    const { SchedulerModel, queryLoading } = this.props;
+    const columns = [
+      { title: '调度器名称', dataIndex: 'schedName' },
+      { title: '调度器实例', dataIndex: 'instanceName' },
+      { title: '最后健康检查时间', dataIndex: 'lastCheckinTime' },
+      { title: '健康检查间隔', dataIndex: 'checkinInterval' },
+      {
+        title: '操作', align: 'center', key: 'action',
+        render: () => (
+          <Fragment>
+            <a>详情</a>
+          </Fragment>
+        ),
+      },
+    ];
+    return (
+      <Table
+        size="middle"
+        bordered={true}
+        rowKey={record => record.instanceName}
+        columns={columns}
+        loading={queryLoading}
+        dataSource={SchedulerModel.data}
+        pagination={ModelInitState.pagination}
+      />
+    );
+  }
 
   render() {
     return (
       <PageHeaderLayout>
         <Card bordered={false}>
-          Scheduler
+          <div>
+            {this.queryForm()}
+          </div>
+          {/* {this.actionButton()} */}
+          {this.getTable()}
         </Card>
       </PageHeaderLayout>
     );
